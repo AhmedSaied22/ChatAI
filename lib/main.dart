ea668d968169a217aff4bc356b8fa3e0a1136150
Ahmed Saied <ahmedsaied2016201@gmail.com>
2024-03-03 16:24:54 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ChatAiApp());
+}
+
+class ChatAiApp extends StatelessWidget {
+  const ChatAiApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
