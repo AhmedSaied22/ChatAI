88aed85d080426ce3fc84b061ee661aa295ae36b
AhmedSaied22 <ahmedsaied2016201@gmail.com>
2024-03-05 15:24:08 +0200

    add firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:chat_ai/firebase_options.dart';
 import 'package:chat_ai/views/home_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ChatAiApp());
 }
 
